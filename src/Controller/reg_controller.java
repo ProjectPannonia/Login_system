0aae575d3f513256059701b3966cb4001463e794
Adam Letenyei <letenyeidam89@gmail.com>
2019-11-04 21:55:48 +0100

    registration gui version 1.0 working

--- src/Controller/reg_controller.java
+++ src/Controller/reg_controller.java
@@ -1,5 +1,10 @@
 package Controller;
 
-public class reg_controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
+
+public class reg_controller {
+    @FXML
+    TextField vezeteknev_tb, keresztnev_tb, telepules_tb,iranyitoszam_tb,utcanev_tb,kozteruletjelleg_tb,email_tb;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+
+
+    public void start(Stage primaryStage) throws  Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Design/registration_page.fxml"));
+        primaryStage.setTitle("Registration page");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
