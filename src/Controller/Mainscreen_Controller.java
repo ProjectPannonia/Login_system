e0cfed031fff79f36a06afc064315285012b0323
Adam Letenyei <letenyeidam89@gmail.com>
2019-11-12 20:03:56 +0100

    Mainscreen_controller gui created

--- src/Controller/Mainscreen_Controller.java
+++ src/Controller/Mainscreen_Controller.java
@@ -1,5 +1,9 @@
 package Controller;
 
-public class Mainscreen_Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class Mainscreen_Controller {
+    @FXML
+    Button login_bt,registration_bt,forgot_bt;
 }
